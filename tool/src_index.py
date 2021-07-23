d09904a5727755ff838f70e0d26e969e4d428ea4
maxchen32 <maxchen32@qq.com>
2021-07-23 21:13:01 +0800

    updated21:13:01

--- tool/src_index.py
+++ tool/src_index.py
@@ -12,12 +12,12 @@ l.remove("index.html")
 HEAD = """<!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="../css/homepage.css" />
+    <link rel="stylesheet" type="text/css" href="../css/toc.css">
     <script type="text/javascript" src="../js/viewLink.js"></script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no">
 </head>
-<body onload="viewLink()" style="background-color: #ddffff;">
+<body onload="viewLink()">
 <ul>"""
 REAR = """</ul>
 </body>
@@ -26,7 +26,7 @@ REAR = """</ul>
 f.write(HEAD)
 for i in l:
 	if (i[:4] != "test" and i[:3] != "tmp"):
-		f.write('<li><a href="' +i+ '" class="uri">' +i+ '</a></li> <br />\n')
+		f.write('<li><a href="' +i+ '" class="uri">' +i+ '</a></li>\n')
 f.write(REAR)
 f.close()
 

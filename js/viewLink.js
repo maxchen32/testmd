61f20b20f056e5498d092723e332b6d96bb9bffe
maxchen32 <maxchen32@qq.com>
2022-03-19 12:09:06 +0800

    clean

--- js/viewLink.js
+++
@@ -1,6 +0,0 @@
-function viewLink(){
-	var sd = document.getElementsByClassName("uri");
-	for (var i = 0; i < sd.length; i++) {
-		sd[i].target = "view_frame";
-	}
-}
--- tool/push.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-import time
-import os
-os.system("python src_index.py")
-os.system("git add --all")
-os.system("git commit -m updated"+time.strftime("%H:%M:%S"))
-os.system("git push origin master")
